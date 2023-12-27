cf56f21c17746cbcd002bb7a06a77dabb342e21d
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-12-27 22:52:25 +0545

    header part ui done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Toggle menu
+
 const toggleMenu = document.getElementById("menu");
 const sidebar = document.getElementById("sidebar-wrapper");
 const navTitle = document.querySelector(".nav-title");
@@ -5,4 +7,13 @@ const navTitle = document.querySelector(".nav-title");
 toggleMenu.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   navTitle.classList.toggle("hide");
+});
+
+// User initials show/hide
+const userInitials = document.getElementById("user-name");
+const userDetails = document.getElementById("user-details");
+
+userInitials.addEventListener("click", () => {
+  userDetails.classList.toggle("hide");
+  userDetails.classList.toggle("show");
 });
